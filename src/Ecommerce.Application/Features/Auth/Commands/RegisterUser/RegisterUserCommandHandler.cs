77bbe23b74e0517367fc994a8fdc9fbda4e5c94b
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2026-06-17 23:00:21 -0600

    Add: create Auth Commands, Handlers and Validations

---
+++ src/Ecommerce.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using Ecommerce.Application.Common.Errors;
+using Ecommerce.Application.Common.Interfaces;
+using Ecommerce.Application.Common.Models;
+using Ecommerce.Application.DTOs.Auth;
+using Ecommerce.Domain.Entities;
+using MediatR;
+
+namespace Ecommerce.Application.Features.Auth.Commands.RegisterUser;
+
+public sealed class RegisterUserCommandHandler
+    : IRequestHandler<RegisterUserCommand, Result<AuthenticationResponse>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHashingService _passwordHashingService;
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterUserCommandHandler(
+        IUserRepository userRepository,
+        IPasswordHashingService passwordHashingService,
+        IJwtTokenGenerator jwtTokenGenerator,
+        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _passwordHashingService = passwordHashingService;
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<AuthenticationResponse>> Handle(
+        RegisterUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var emailExists = await _userRepository.EmailExistsAsync(
+            request.Email,
+            cancellationToken);
+
+        if (emailExists)
+        {
+            return Result<AuthenticationResponse>.Failure(AuthErrors.EmailAlreadyExists);
+        }
+
+        var user = new User(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            "TEMPORARY_PASSWORD_HASH");
+
+        var passwordHash = _passwordHashingService.HashPassword(
+            user,
+            request.Password);
+
+        user.ChangePasswordHash(passwordHash);
+
+        await _userRepository.AddAsync(user, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        var accessToken = _jwtTokenGenerator.GenerateToken(user);
+
+        var response = new AuthenticationResponse(
+            user.Id,
+            user.Email,
+            accessToken);
+
+        return Result<AuthenticationResponse>.Success(response);
+    }
+}
---
+++ src/Ecommerce.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+
+namespace Ecommerce.Application.Features.Auth.Commands.RegisterUser;
+
+public sealed class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(command => command.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required.")
+            .MaximumLength(100)
+            .WithMessage("First name must not exceed 100 characters.");
+
+        RuleFor(command => command.LastName)
+            .NotEmpty()
+            .WithMessage("Last name is required.")
+            .MaximumLength(100)
+            .WithMessage("Last name must not exceed 100 characters.");
+
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email format is invalid.")
+            .MaximumLength(150)
+            .WithMessage("Email must not exceed 150 characters.");
+
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage("Password is required.")
+            .MinimumLength(8)
+            .WithMessage("Password must have at least 8 characters.")
+            .Matches("[A-Z]")
+            .WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]")
+            .WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]")
+            .WithMessage("Password must contain at least one number.");
+    }
+}
